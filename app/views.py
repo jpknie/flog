cc52470b6e889ddecefa07056b1fd5d667f55937
Jani Nieminen <jpkniem@gmail.com>
2013-09-05 21:03:53 +0300

    Apply flask login extension in the views. Require login for viewing the entries.

--- app/views.py
+++ app/views.py
@@ -1,12 +1,23 @@
-from app import app
+from app import app, lm
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from flask import render_template, Flask, request, session, g, redirect, url_for, \
 	abort, flash
-
+from forms import LoginForm
 from models import *
+
+@lm.user_loader
+def load_user(userid):
+	return User.query.get(int(userid))
+
+@app.before_request
+def before_request():
+	g.user = current_user
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
+	user = g.user
 	page_title = "Flog 0.01"
 	entries = [
 		{
@@ -24,20 +35,19 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  error = None
-  if request.method == 'POST':
-	if request.form['username'] != app.config['USERNAME']:
-		error = 'Invalid Username'
-	elif request.form['password'] != app.config['PASSWORD']:
-		error = 'Invalid password'
-	else:
-		session['logged_in'] = True
-		flash('You were logged in')
-		return redirect(url_for('show_entries'))
-  return render_template('login.html', error=error)
+	form = LoginForm(request.form)
+	if form.validate_on_submit():
+		user = User.query.filter_by(login_name = form.login_name.data).first()
+		if user and (user.password == form.password.data):
+			session['user_id'] = user.user_id
+			flash('Welcome %s' % user.login_name)
+			return redirect(url_for('index'))
+		flash('Wrong email or password', 'error-message')
+	return render_template('login.html', form=form)
 
 @app.route('/logout')
+@login_required
 def logout():
-  session.pop('logged_in', None)
-  flash('You were logged out')
-  return redirect(url_for('show_entries'))
+	logout_user()
+	flash('You were logged out')
+	return redirect(url_for('login'))
