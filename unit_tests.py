24161b06d2683758433b0fa981d80f1bc72be390
Jani Nieminen <jpkniem@gmail.com>
2013-09-12 18:56:49 +0300

    Include unit test suite for the app.

---
+++ unit_tests.py
@@ -0,0 +1,35 @@
+#!flask/bin/python
+
+import os
+import unittest
+
+from app import app
+from app.database import db
+from app.models import *
+
+class TestUser(unittest.TestCase):
+	def setUp(self):
+		app.config['TESTING'] = True
+		app.config['CSRF_ENABLED'] = False
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://janiniem:devpass@localhost/flog'
+		self.app = app.test_client()
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+
+class TestEntry(unittest.TestCase):
+	def setUp(self):
+		app.config['TESTING'] = True
+		app.config['CSRF_ENABLED'] = False
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://janiniem:devpass@localhost/flog'
+		self.app = app.test_client()
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+
+if __name__ == 'main':
+	unittest.main()
