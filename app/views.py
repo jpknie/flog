b77a15d1fddb3f02547506b7618d97674be972a4
Jani Nieminen <jpkniem@gmail.com>
2013-09-12 18:54:25 +0300

    Make new views for tag editor and listing. Rename index -view as entries.

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from app import app, lm
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from flask import render_template, Flask, request, session, g, redirect, url_for, \
 	abort, flash
-from forms import LoginForm, EntryForm
+from forms import LoginForm, EntryForm, TagForm
 from models import *
 
 import datetime
@@ -16,13 +16,31 @@ def before_request():
 	g.user = current_user
 
 @app.route('/')
-@app.route('/index')
-@login_required
-def index():
+@app.route('/entries')
+def entries():
 	user = g.user
 	page_title = "Flog 0.01"
 	entries = Entry.query.options(db.joinedload('author')).order_by(Entry.id)
-	return render_template('index.html', page_title = page_title, entries = entries)
+	return render_template('entries.html', page_title = page_title, entries = entries)
+
+@app.route('/add_tag', methods=['GET', 'POST'])
+@login_required
+def add_tag():
+	form = TagForm(request.form)
+	page_title = 'Add new tag'
+	return render_template('tag_editor.html', form_action = 'add_tag', form = form, page_title = page_title)
+
+@app.route('/edit_tag', methods=['GET', 'POST'])
+@login_required
+def edit_tag(tagid):
+	tag = Tag.query.get(int(tagid))
+	return render_template('tag_editor.html', form_action = 'edit_tag', form = form, page_title = page_title)
+
+@app.route('/tags')
+def tags():
+	page_title = 'Tags'
+	tags = Tag.query.all()
+	return render_template('tags.html', page_title = page_title, tags = tags)
 
 @app.route('/add_entry', methods=['GET', 'POST'])
 @login_required
@@ -36,7 +54,7 @@ def add_entry():
 		form.populate_obj(entry)
 		db.session.add(entry)
 		db.session.commit()
-		return redirect(url_for('index'))
+		return redirect(url_for('entries'))
 	return render_template('entry_editor.html', form_action = 'add_entry', form = form, page_title = page_title)
 
 @app.route('/edit_entry/<int:entryid>', methods=['GET', 'POST'])
@@ -45,7 +63,7 @@ def edit_entry(entryid):
 	entry = Entry.query.get(int(entryid))
 	if entry.user_id != g.user.user_id:
 		flash("Cannot edit this entry.", 'error-message')
-		return redirect(url_for('index'))
+		return redirect(url_for('entries'))
 	form = EntryForm(request.form, obj=entry)
 	page_title = 'Edit blog entry'
 	if request.method == 'POST' and form.validate_on_submit():
@@ -54,7 +72,7 @@ def edit_entry(entryid):
 		form.user_id = g.user.user_id
 		db.session.add(entry)
 		db.session.commit()
-		return redirect(url_for('index'))
+		return redirect(url_for('entries'))
 	return render_template('entry_editor.html', form_action = 'edit_entry', form = form, page_title = page_title)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -65,7 +83,7 @@ def login():
 		if user and (user.password == form.password.data):
 			session['user_id'] = user.user_id
 			flash('Welcome %s' % user.login_name)
-			return redirect(url_for('index'))
+			return redirect(url_for('entries'))
 		flash('Wrong email or password', 'error-message')
 	return render_template('login.html', form=form)
 
