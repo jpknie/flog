608f10d507322c10ca0ec4b5afcae88404d41f28
Jani Nieminen <jpkniem@gmail.com>
2013-09-19 19:37:00 +0300

    Tags must be unique.

--- app/models.py
+++ app/models.py
@@ -59,7 +59,5 @@ class Tag(db.Model):
 	__tablename__ = 'tag'
 
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(200))
+	name = db.Column(db.String(200), unique=True, nullable=False)
 
-	def __init__(self, name):
-		self.name = name
